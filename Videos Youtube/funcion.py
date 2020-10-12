07f0505021f671ac4af6aace25845c711c9c8d5e
carlosdiazz <c.diazadriann@gmail.com>
2020-10-12 12:41:55 -0400

    Agregue mas funciones

--- Videos Youtube/funcion.py
+++ Videos Youtube/funcion.py
@@ -1,19 +1,42 @@
 import os
-os.system("cls")
 
 def main():
-    
-    print(digital_root(493193 ))
-    #print(to_camel_case("A-B-C"))
-    #print(xo("xooxx"))
-    #print(xo("ooxXm"))
-    #print(xo("zpzpzpp"))
-    #print(xo("zzoo"))
-    
-    #lista=[1,2,'aasf','1','123',123,14] 
-    #print(sacar_numero(lista))
-    #numeros_al100()
-    #impimir()
+    hacer=int(input("Ingrese que quieres hacer: "))
+    while True:
+        os.system("cls")       
+
+        if hacer == 0:
+            break
+
+        elif hacer == 1:
+            palabra=input("Ingrese una palabra para saber su composicion: ")
+            repetir(palabra)
+        
+        elif hacer == 2:
+            palabra=input("Ingrese una palabra para saber la palabra que se repite: ")
+            print(saber_repetidas(palabra))
+        
+        elif hacer == 3:
+            print(friend(["Ryan", "Kieran", "Mark","jose","Maria","Cecilia","Carlos"]))
+
+        elif hacer == 4:
+            print(digital_root(132189 ))
+            print(digital_root(493193 ))
+        
+        elif hacer == 5:
+            print(to_camel_case("A-B-C"))
+        
+        elif hacer == 6:
+            print(xo("xooxx"))
+            print(xo("ooxXm"))
+            print(xo("ooxXm"))
+            print(xo("zzoo"))
+        
+        elif hacer == 7:
+            lista=[1,2,'aasf','1','123',123,14] 
+            print(sacar_numero(lista))
+        
+        hacer=int(input("Ingrese que quieres hacer: "))
 
 def numeros_al100():
     i=0
@@ -103,15 +126,52 @@ def digital_root(n):
         
     return solo
 
+#Esta funcion sacas solo la palabras de 4 digitos de una lista
+def friend(x):
+    nueva=[]
+    for i in x:
+        saber=len(i)
+        if saber == 4:
+            nueva.append(i)
+    return nueva
 
+#Esta funcion va a descomponer una palabra y decir cuantas veces se repite, en caso de que se repita
+def repetir(palabra):
+    nueva=list(palabra.lower())
+    palabra2=""
 
+    print(f"La palabra que ingreso es: {palabra}")
+    for i in palabra:
 
-
-
-
-
+        if i not in palabra2:
+            palabra2=palabra2.lower()+i
+    
+    for i in palabra2:
         
+        saber_si_es_una=nueva.count(i)
+        if saber_si_es_una == 1:
+            print(f"La letra {i} se encuentra {nueva.count(i)} vez")
+        else:
+            print(f"La letra {i} se encuentra {nueva.count(i)} veces")
 
+#Este algortimo, solo imprime si una palabra se repite, e indica cual palabra se repitio
+def saber_repetidas(palabra):
+    nueva=list(palabra.lower())
+    salida=""
 
+    repetida=0
 
+    for i in palabra:
+        saber=nueva.count(i)
+        if saber>=2:
+                 
+            if i not in salida:
+                repetida=repetida+1
+                salida=salida+i
+
+    if salida=="":
+        return repetida
+    else: 
+        return repetida,salida
+  
 main()
